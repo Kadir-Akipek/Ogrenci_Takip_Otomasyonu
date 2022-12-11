85a5a930987e000463bef9e901477db9d9d5ed80
akipekkadir23@gmail.com <akipekkadir23@gmail.com>
2022-12-11 17:38:19 +0300

    Add project files.

---
+++ proje2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace proje2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_giris = new System.Windows.Forms.Button();
+            this.txt_kullaniciadi = new System.Windows.Forms.TextBox();
+            this.txt_sifre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_giris.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_giris.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_giris.Location = new System.Drawing.Point(482, 334);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(123, 59);
+            this.btn_giris.TabIndex = 0;
+            this.btn_giris.Text = "Giriş Yap";
+            this.btn_giris.UseVisualStyleBackColor = false;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click_1);
+            // 
+            // txt_kullaniciadi
+            // 
+            this.txt_kullaniciadi.Location = new System.Drawing.Point(575, 198);
+            this.txt_kullaniciadi.Name = "txt_kullaniciadi";
+            this.txt_kullaniciadi.Size = new System.Drawing.Size(150, 22);
+            this.txt_kullaniciadi.TabIndex = 1;
+            // 
+            // txt_sifre
+            // 
+            this.txt_sifre.Location = new System.Drawing.Point(575, 247);
+            this.txt_sifre.Name = "txt_sifre";
+            this.txt_sifre.Size = new System.Drawing.Size(150, 22);
+            this.txt_sifre.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(416, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(416, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(190, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(416, 38);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Öğrenci Takip Otomasyonu";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(66, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 206);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 411);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kullanıcı Adı: administrator";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 440);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Şifre: 1234";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(840, 522);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_sifre);
+            this.Controls.Add(this.txt_kullaniciadi);
+            this.Controls.Add(this.btn_giris);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_giris;
+        private System.Windows.Forms.TextBox txt_kullaniciadi;
+        private System.Windows.Forms.TextBox txt_sifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ proje2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_giris_Click_1(object sender, EventArgs e)
+        {
+            if (txt_kullaniciadi.Text == "administrator" && txt_sifre.Text == "1234")
+            {
+                MessageBox.Show("Hoş Geldiniz");
+                Form2 yeni1 = new Form2();
+                yeni1.Show();
+                this.Hide();
+            }
+            if (txt_kullaniciadi.Text == "" || txt_sifre.Text == "")
+            {
+                MessageBox.Show("Lütfen Kullanıcı Adı ve Şifre alanlarını boş bırakmayınız");
+            }
+            else if (txt_kullaniciadi.Text != "administrator" || txt_sifre.Text != "1234")
+            {
+                MessageBox.Show("Kullanıcı Adı veya Şifre hatalı lütfen tekrar deneyin");
+            }
+        }
+    }
+}
